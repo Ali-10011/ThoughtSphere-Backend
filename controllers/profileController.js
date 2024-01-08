09c08807a70da62754ca5fbaa3f5b9d79818bccb
Hassaan-T075 <twincast00@gmail.com>
2024-01-08 06:15:11 +0500

    fix: modify update profile controller to update social links

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,20 +4,21 @@ const authModel = require("../models/authModel");
 
 const updateProfile = async (req, res) => {
   try {
-    const profile = await profileModel.findOneAndUpdate({ email: req.email }, {username: req.body.username});
-    const auth = await authModel.findOneAndUpdate({ email: req.email }, {username: req.body.username});
+    console.log(req.body.social)
+    const profile = await profileModel.findOneAndUpdate({ email: req.email }, { username: req.body.username, social: req.body.social });
+    console.log(profile)
+    const auth = await authModel.findOneAndUpdate({ email: req.email }, { username: req.body.username });
     if (profile && auth) {
-      
-        return res.status(200).json({code: "1", msg : "Profile Updated"});
-      }
-      else 
-      {
-        return res.status(200).json({code: "0", msg: "Profile Not Updated" });
-      }
-       
-    
+
+      return res.status(200).json({ code: "1", msg: "Profile Updated" });
+    }
+    else {
+      return res.status(200).json({ code: "0", msg: "Profile Not Updated" });
+    }
+
+
   } catch (e) {
-    return res.status(500).json({ code: "0",msg: "Internal server error" });
+    return res.status(500).json({ code: "0", msg: "Internal server error" });
   }
 };
 
@@ -25,9 +26,9 @@ const getProfile = async (req, res) => {
   try {
     const profile = await profileModel.findOne({ email: req.email });
 
-    return res.status(200).json({code: "1", profile});
+    return res.status(200).json({ code: "1", profile });
   } catch (e) {
-    return res.status(500).json({ code: "0",msg: "Internal server error" });
+    return res.status(500).json({ code: "0", msg: "Internal server error" });
   }
 };
 
@@ -35,9 +36,9 @@ const getUserProfile = async (req, res) => {
   try {
     const profile = await profileModel.findOne({ email: req.body.email });
 
-    return res.status(200).json({code: "1", profile});
+    return res.status(200).json({ code: "1", profile });
   } catch (e) {
-    return res.status(500).json({ code: "0",msg: "Internal server error" });
+    return res.status(500).json({ code: "0", msg: "Internal server error" });
   }
 };
 
@@ -45,20 +46,19 @@ const followProfile = async (req, res) => {
   try {
     const user_following = await profileModel.findOne({ email: req.email }); //user who is following
     const user_follower = await profileModel.findOne({ email: req.body.user_follow }); //user who is being follow/unfollow
-   
+
     let followings = user_following.followings;
     let followers = user_follower.followers;
     let msg;
-    
-    if(followings.includes(req.body.user_follow) && followers.includes(req.email))
-    {
-     
-        followings.pop(req.body.user_follow)
-        followers.pop(req.email)
-        msg = "User Unfollowed"
-        console.log(msg)
+
+    if (followings.includes(req.body.user_follow) && followers.includes(req.email)) {
+
+      followings.pop(req.body.user_follow)
+      followers.pop(req.email)
+      msg = "User Unfollowed"
+      console.log(msg)
     }
-   else {
+    else {
       followings.push(req.body.user_follow)
       followers.push(req.email)
       msg = "User Followed"
@@ -71,11 +71,11 @@ const followProfile = async (req, res) => {
     await profileModel.findOneAndUpdate({ email: req.email }, user_following); //user who is following
     await profileModel.findOneAndUpdate({ email: req.body.user_follow }, user_follower); //user who is being follow/unfollow
 
-    return res.status(200).json({code: "1", msg: msg });
-    
+    return res.status(200).json({ code: "1", msg: msg });
+
   } catch (e) {
-   
+
     return res.status(500).json({ code: "0", msg: "Internal server error" });
   }
 };
-module.exports = { updateProfile, getProfile, followProfile , getUserProfile};
+module.exports = { updateProfile, getProfile, followProfile, getUserProfile };
